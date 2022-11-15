99f386a6d754e221345747b8805bba94d0d896c9
hamidubc <80303628+hamidubc@users.noreply.github.com>
2022-11-14 18:29:50 -0800

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -5,7 +5,7 @@ class TestNDVI(unittest.TestCase):
         from calculate import calculate
 
         # Test NDVI calculation when NIR and Red bands are equal
-        self.assertAlmostEqual(calculate(band_nir,band_red), 0)
+        #self.assertAlmostEqual(calculate(band_nir,band_red), 0)
         self.assertAlmostEqual(calculate(1,1), 0)
         self.assertAlmostEqual(calculate(-1,-1), 0)
 
